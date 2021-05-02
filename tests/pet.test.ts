8fac803c355ae1569f9ca44bc8cd3b04ceb029be
SashaJson <sashamiller666@gmail.com>
2021-05-02 23:03:46 +0300

    added in suit pet method: add, update and delete

--- tests/pet.test.ts
+++ tests/pet.test.ts
@@ -7,8 +7,8 @@ describe('User can', function () {
 
     it('receive pet by his id', async function () {
 
-        const body = await pet.getById(9222968140498399655);
-        assert(body.id == 9222968140498399655);
+        const body = await pet.getById(9222968140498401472);
+        assert(body.id == 9222968140498401472);
 
     });
 
@@ -35,7 +35,66 @@ describe('User can', function () {
 
         const body = await pet.findByTags('string');
         assert(body.length > 0);
-        assert(body.some((pet: any) => pet.tags.some((tag: any) => tag.name == 'string')));
+        assert(body.every((pet: any) => pet.tags.some((tag: any) => tag.name == 'string')));
+
+    });
+
+    it('can be added, updated and deleted', async function () {
+
+        const petToCreate = {
+            "category": {
+                "id": 0,
+                "name": "string"
+            },
+            "name": "Cat",
+            "photoUrls": [
+                "http:test.com/image.jpg"
+            ],
+            "tags": [
+                {
+                    "id": 0,
+                    "name": "string",
+                }
+            ],
+            "status": "available",
+        };
+
+        const addedPet = await pet.addNew(petToCreate);
+        assert.deepEqual(addedPet, {
+            ...petToCreate,
+            id: addedPet.id
+        }, 'first ASAAAAAAAP');
+
+        const foundAddPet = await pet.getById(addedPet.id);
+        assert.deepEqual(foundAddPet, {
+            ...petToCreate,
+            id: addedPet.id
+        }, 'SECOND ASAAAAAAAP');
+
+        const newerPet = {
+            id: addedPet.id,
+            category: {
+                id: 1,
+                name: "string2"
+            },
+            name: "Dog",
+            photoUrls: [
+                "http:test.com/image2.jpg"
+            ],
+            tags: [
+                {
+                    id: 1,
+                    name: "string2",
+                }
+            ],
+            status: "pending",
+        };
+
+        const updatePet = await pet.update(newerPet);
+        assert.deepEqual(updatePet, newerPet, 'THIIRD ASAAAAAAAP');
+
+        // TODO: assert 404 error
+        await pet.delete(addedPet.id);
 
     });
 
