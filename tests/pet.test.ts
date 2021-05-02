b2ca97492d318d21f6c14f64b7339f6db4ad0ed2
SashaJson <sashamiller666@gmail.com>
2021-05-02 19:57:59 +0300

    added some its test in suit pet

--- tests/pet.test.ts
+++ tests/pet.test.ts
@@ -1,14 +1,57 @@
 import got from 'got'
 import {strict as assert} from 'assert';
+import {URLSearchParams} from "url";
 
 describe('User can', function () {
 
-    it('receive pet by his id',async function () {
+    it('receive pet by his id', async function () {
 
         const response = await got('https://petstore.swagger.io/v2/pet/9222968140498399655');
         const body = JSON.parse(response.body);
 
         assert(body.id == 9222968140498399655, `ASAAP`);
+
+    });
+
+    it('can be received by status', async function () {
+
+        let response = await got('https://petstore.swagger.io/v2/pet/findByStatus', {
+            searchParams: {status: 'available'}
+        });
+        let body = JSON.parse(response.body);
+        assert(body.length > 0);
+
+        response = await got('https://petstore.swagger.io/v2/pet/findByStatus', {
+            searchParams: {status: 'pending'}
+        });
+        body = JSON.parse(response.body);
+        assert(body.length > 0);
+
+        response = await got('https://petstore.swagger.io/v2/pet/findByStatus', {
+            searchParams: {status: 'sold'}
+        });
+        body = JSON.parse(response.body);
+        assert(body.length > 0);
+
+        response = await got('https://petstore.swagger.io/v2/pet/findByStatus', {
+            searchParams: new URLSearchParams({status: ['pending', 'available']})
+        });
+        body = JSON.parse(response.body);
+        assert(body.length > 0);
+        assert(body.some((pet: any) => pet.status == 'available'));
+        assert(body.some((pet: any) => pet.status == 'pending'));
+        assert(!body.some((pet: any) => pet.status == 'sold'));
+
+    });
+
+    it('can be received by tag', async function () {
+
+        const response = await got('https://petstore.swagger.io/v2/pet/findByTags', {
+            searchParams: {tags: 'string'}
+        });
+        const body = JSON.parse(response.body);
+        assert(body.length > 0);
+        assert(body.some((pet: any) => pet.tags.some((tag: any) => tag.name == 'string')));
 
     });
 
