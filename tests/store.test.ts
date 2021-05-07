a3a1ef52643f1474d5d39d675af654a759005139
SashaJson <sashamiller666@gmail.com>
2021-05-07 20:59:46 +0300

    added to store test api client

--- tests/store.test.ts
+++ tests/store.test.ts
@@ -1,27 +1,26 @@
-import { strict as assert } from 'assert';
-import { PetController } from '../api/controller/pet.controller';
-import { StoreController } from '../api/controller/store.controller';
 import { definitions } from '../.temp/types';
-
-const pet = new PetController();
-const store = new StoreController();
+import { strict as assert } from 'assert';
+import { ApiClient } from '../api/client';
 
 describe('Store', function () {
 
     it('should return his inventory, and correctly updates statuses', async function () {
-        const inventory = await store.getInventory();
+
+        const adminClient = await ApiClient.loginAs({ username: 'admin', password: 'admin'});
+
+        const inventory = await adminClient.store.getInventory();
         assert(Object.keys(inventory).length > 0);
 
-        await pet.addNew(petWithStatus('sold'));
-        const inventoryWithSoldAdded = await store.getInventory();
-        assert.equal(inventoryWithSoldAdded.available, inventory.available + 1);
+        await adminClient.pet.addNew(petWithStatus('sold'));
+        const inventoryWithSoldAdded = await adminClient.store.getInventory();
+        assert.equal(inventoryWithSoldAdded.available, inventory.available);
 
-        await pet.addNew(petWithStatus('available'));
-        const inventoryWithAvailableAdded = await store.getInventory();
+        await adminClient.pet.addNew(petWithStatus('available'));
+        const inventoryWithAvailableAdded = await adminClient.store.getInventory();
         assert.equal(inventoryWithAvailableAdded.available, inventory.available + 1);
 
-        await pet.addNew(petWithStatus('pending'));
-        const inventoryWithPendingAdded = await store.getInventory();
+        await adminClient.pet.addNew(petWithStatus('pending'));
+        const inventoryWithPendingAdded = await adminClient.store.getInventory();
         assert.equal(inventoryWithPendingAdded.available, inventory.available + 1);
     });
 
