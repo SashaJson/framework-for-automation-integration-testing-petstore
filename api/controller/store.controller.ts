3e8dc34961711cdbafba4d7a1ff58afd8fe8c481
SashaJson <sashamiller666@gmail.com>
2021-05-07 21:02:34 +0300

    added in all method headers store controller

--- api/controller/store.controller.ts
+++ api/controller/store.controller.ts
@@ -9,6 +9,7 @@ export class StoreController extends BaseController {
         return (
             await new JsonRequest()
                 .url('http://93.126.97.71:10080/api/store/inventory')
+                .headers({ token: this.params.token })
                 .send<operations['getInventory']['responses']['200']['schema']>()
         ).body
     }
