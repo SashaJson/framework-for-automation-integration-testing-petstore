0934afd4acd95a252f0ea49d7f0a69e121441b9e
SashaJson <sashamiller666@gmail.com>
2021-05-07 21:25:43 +0300

    added new method to store controller

--- api/controller/store.controller.ts
+++ api/controller/store.controller.ts
@@ -1,10 +1,31 @@
 import { JsonRequest } from '../request';
-import { operations } from '../../.temp/types';
+import { operations, definitions } from '../../.temp/types';
 import { BaseController } from './base.controller';
 
 // TODO ADD TO METHOD VALIDATE JSON-SCHEMA
 
 export class StoreController extends BaseController {
+
+    async getOrderById(id: number | string) {
+        return (
+            await new JsonRequest()
+                .url(`http://93.126.97.71:10080/api/store/order/${id}`)
+                .headers({ token: this.params.token })
+                .send<operations['getOrderById']['responses']['200']['schema']>()
+        )
+    }
+
+    async placeOrder(order: Omit<definitions['Order'], 'id'>) {
+        return (
+            await new JsonRequest()
+                .url('http://93.126.97.71:10080/api/store/order')
+                .headers({ token: this.params.token })
+                .method('POST')
+                .body(order)
+                .send<Required<operations['placeOrder']['responses']['200']['schema']>>()
+        ).body
+    }
+
     async getInventory() {
         return (
             await new JsonRequest()
