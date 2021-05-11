dd83ccc7e5eba70aaac5c5407eb0ac2bcd7296cf
SashaJson <sashamiller666@gmail.com>
2021-05-12 02:04:58 +0300

    updated constructor in client.ts

--- api/client.ts
+++ api/client.ts
@@ -1,20 +1,24 @@
 import { PetController } from './controller/pet.controller';
 import { StoreController } from './controller/store.controller';
 import { UserController } from './controller/user.controller';
+import {ControllerOptions} from './controller/base.controller';
 import { CookieJar } from 'tough-cookie';
+import {CONFIG} from '../config/env';
 
 export class ApiClient {
     public readonly pet: PetController;
     public readonly store: StoreController;
     public readonly user: UserController;
 
-    constructor(params?: { token?: string, cookies?: CookieJar }) {
-        const defaultParams = {
-            cookies: new CookieJar()
+    constructor(options?: Partial<ControllerOptions>) {
+        const defaultOptions = {
+            cookieJar: new CookieJar(),
+            prefixUrl: CONFIG.PETSTORE_URL,
+            prefixPath: CONFIG.PETSTORE_API_PREFIX_PATH
         };
         const mergeParams = {
-            ...defaultParams,
-            ...params
+            ...defaultOptions,
+            ...options
         };
 
         this.pet = new PetController(mergeParams);
